5b760ac1c012cf71d57c0d5a92afada93390dd1e
RealMag777 <RealMag777@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-02 10:38:36 +0000

    removing old version of the plugin
    git-svn-id: https://plugins.svn.wordpress.org/woocommerce-products-filter/trunk@1191174 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- views/html_types/color.php
+++
@@ -1,2 +0,0 @@
-<?php if (!defined('ABSPATH')) die('No direct access allowed'); ?>
-This feature is in the <a href="http://codecanyon.net/item/woocommerce-products-filter-light/11498469?ref=realmag777" target="_blank">premium version only</a><br />
